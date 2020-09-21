3b82ba2a876557bd643eb8f99297e0c0b7209e9f
aloveland <44244521+aloveland@users.noreply.github.com>
2020-09-21 19:55:06 -0400

    Update task.js

--- practice/task1/task.js
+++ practice/task1/task.js
@@ -32,7 +32,7 @@ const query2 = "SELECT title FROM movies where title LIKE '%e' ORDER BY title";
 //      Comedy      |           370.6
 //      Animation   |           968.5
 //      Action      |         3853.27
-const query3 = "SELECT SUM(*) from boxoffice as total_boxoffice FROM movies GROUP BY genre ORDER BY SUM(*) asc";
+const query3 = "";
 //
 // query 4: print out the title and boxoffice of the movie with the highest boxoffice.
 //
