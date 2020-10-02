59e6f9725f4ba223920f66a94ef507de67da679d
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-02 14:51:14 -0400

    Update server4.js

--- practice/task4/server4.js
+++ practice/task4/server4.js
@@ -118,7 +118,7 @@ app.get('/search', async (req, res) => {
     
     // TODO
     try {
-      const query = "SELECT name, location, maxlength FROM camgrounds where name is $1";
+      const query = "SELECT name, location, maxlength FROM camgrounds where name = $1";
       const dbresponse = await pool.query(query, [req.query.name]);
       console.log(dbresponse);
        //const dbresponse = await pool.query(query);
