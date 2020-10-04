ead8f65372fbe349e8cab540d2f27130964174e2
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-04 15:46:25 -0400

    Update workshop.sql

--- workshop-server/workshop.sql
+++ workshop-server/workshop.sql
@@ -1,6 +1,6 @@
 \c postgres
-DROP DATABASE IF EXISTS workshopV1;
-CREATE DATABASE workshopV1;
+DROP DATABASE IF EXISTS workshopONE;
+CREATE DATABASE workshopONE;
 \c workshopV1
 
 CREATE TABLE React Fundementals{
