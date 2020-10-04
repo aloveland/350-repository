129dfb961945e682a0a8e069a61993f91b52fd1f
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-04 16:34:10 -0400

    Update workshop.sql

--- workshop-server/workshop.sql
+++ workshop-server/workshop.sql
@@ -3,10 +3,10 @@ DROP DATABASE IF EXISTS workshopv1;
 CREATE DATABASE workshopv1;
 \c workshopv1
 
-CREATE TABLE 'React Fundementals'{
+CREATE TABLE 'React Fundementals'(
        id SERIAL PRIMARY KEY,
        name TEXT NOT NULL,
-};
+);
 INSERT INTO 'React Fundementals' (name) VALUES
 ('Ahmed Abdelali'),
 ('Ann Frank'),
@@ -19,10 +19,10 @@ INSERT INTO 'React Fundementals' (name) VALUES
 ('Samantha Eggert'),
 ('Tim Smith');
 
-CREATE TABLE HardWorkers{
+CREATE TABLE HardWorkers(
        id SERIAL PRIMARY KEY,
        name TEXT NOT NULL,
-};
+);
 INSERT INTO HardWorkers (name) VALUES
 ('Brad Pitt'),
 ('Leo DiCaprio'),
