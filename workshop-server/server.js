dd934e0b2833de0b9c780bc55132193088341e96
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-04 16:55:23 -0400

    Update server.js

--- workshop-server/server.js
+++ workshop-server/server.js
@@ -15,7 +15,7 @@ const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "workshopV1"
+    database: "workshopv1"
 };
 
 const pool = new Pool(config);
