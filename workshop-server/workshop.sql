558c168f2df99415817d19f72ce9a65a637956b5
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-04 21:45:35 -0400

    Update workshop.sql

--- workshop-server/workshop.sql
+++ workshop-server/workshop.sql
@@ -6,9 +6,9 @@ CREATE DATABASE workshopv1;
 CREATE TABLE workshop(
        id SERIAL PRIMARY KEY,
        name TEXT NOT NULL,
-       workshop TEXT NOT NULL
+       workshopgroup TEXT NOT NULL
 );
-INSERT INTO hardworkers (name, workshop) VALUES
+INSERT INTO hardworkers (name, workshopgroup) VALUES
 ('Brad Pitt', 'hardworkers'),
 ('Leo DiCaprio','hardworkers'),
 ('Ben Affleck','hardworkers'),
