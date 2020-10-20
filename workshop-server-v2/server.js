5c8c1218de637661d333f5c913d456157d198283
aloveland <44244521+aloveland@users.noreply.github.com>
2020-10-20 05:57:42 -0400

    Update server.js

--- workshop-server-v2/server.js
+++ workshop-server-v2/server.js
@@ -174,6 +174,16 @@ app.post("/enroll", async (req, res) => {
             console.log("5");
             res.json({status: 'user not in database, user added'});
         }
+        const workshoptest = "SELECT * FROM workshopinfo WHERE title = $1 AND date = $2 AND location = $3";
+        const workshopresponse= await pool.query(template,[title, date, location]);
+        if(workshopresponse.rowCount == 0){
+            res.json({status: 'workshop does not exist'});
+        }
+        const userinshop = "SELECT * FROM attendees WHERE username = $1 AND title = $2 AND date = $3 AND location = $4";
+        const userinshopresponse = await pool.query(template,[username, title, date, location]);
+        if(userrinshopresponse.rowCount > 0){
+            res.json({status: 'user already enrolled'}):
+        }
   
     } catch (err){
         // whoops
