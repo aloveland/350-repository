20c2f0fc550002b33bdac1451e23ee356f6c41c4
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-04 22:27:18 -0500

    Create data.sql

---
+++ practice/task6/data.sql
@@ -0,0 +1,12 @@
+\c postgres
+DROP DATABASE IF EXISTS taskSix;
+CREATE DATABASE taskSix;
+\c taskSix
+
+CREATE TABLE users(
+  username SERIAL PRIMARY KEY,
+  password TEXT NOT NULL,
+  screenName TEXT NOT NULL,
+)
+INSERT INTO users(username, password, screenName)VALUES
+  ('user1', 'userpass34', 'user example');
