f6e9da80dd02938f2fefd2f01a7ffc0601589a61
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-14 02:58:10 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -1,7 +1,7 @@
 
 import Layout from '../components/MyLayout.js'
 
-export default function campsearch() {
+export default function camp() {
   return (
     <Layout>
       <p>This is the search page</p>
