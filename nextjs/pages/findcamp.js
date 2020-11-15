447b0e24afd29ab087c9aba2bafbfe1bea5ddf1e
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-15 00:32:03 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -4,15 +4,13 @@ import {getCampInfo} from '../lib/utils.js';
 import React from "react";
 
 export default function Camps() {
-  class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { search: "" };
-  }
-  
-  
-  
   return (
+     class Home extends React.Component {
+        constructor(props) {
+        super(props);
+        this.state = { search: "" };
+        }
+    
     <Layout>
       <p>This is the search page</p>
     </Layout>
