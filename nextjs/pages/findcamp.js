f21be2477fa39b102503e4da00f1f29a229d5264
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-15 01:26:20 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -9,6 +9,52 @@ export default function Camps() {
         super(props);
         this.state = { search: "" };
         }
+        
+         handleUpdate(evt) {
+              this.setState({ search: evt.target.value });
+            }
+        
+          async handleSearch(evt) {
+            const campground = await getInfo(this.state.search);
+            console.log(campground);
+            this.setState({ campground });
+          }
+          render() {
+    const that = this;
+    return (
+      <Layout
+        style={{ margin: "auto auto", width: "600px", textAlign: "center" }}
+      >
+        <h2>New Mexico Campground Search</h2>
+        <input
+          type="text"
+          className="text-style"
+          value={this.state.search}
+          onChange={this.handleUpdate.bind(this)}
+        />
+        <br />
+        <br />
+        <div onClick={this.handleSearch.bind(that)} className="button-style">
+          Submit
+        </div>
+        <br /> <br />
+        {this.state.campground ? (
+          <div>
+            <br />
+            <h3>{this.state.campground.name}</h3>
+            <br />
+            <img
+              style={{ maxWidth: "700px", maxHeight: "500px" }}
+              src={this.state.campground.image_url}
+            />
+            <br /> <h4>{this.state.campground.closest_town}</h4>
+            <br />
+            <div className="description">
+              <p>{this.state.campground.description}</p>
+              <br />{" "}
+            </div>
+          </div>
+        ) : null}
   
   return (
    <Layout>
