11ca06cf58ab9866b26d1c5c25232ad299a8d55d
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-16 09:29:38 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -5,7 +5,7 @@ import {getCampInfo} from '../lib/utils.js';
 
   
     
-    <div classname="container">
+    <div classname="container"
      <div style={{ margin: "auto atuo", width: "600px, textAlign: "center"}}
       <p>This is the search page</p>
         <input
@@ -13,6 +13,7 @@ import {getCampInfo} from '../lib/utils.js';
             value = {this.state.search} 
             onChange ={this.handleUpdate.bind(this)}
         />
+    </div>
     </div>
     <div className="button-style" onCLick={this.handleSearch.bind(this)}>Search</div>
 
