1f5add6de38145b3a8f948852b330960a5758f32
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-16 06:32:28 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -3,11 +3,9 @@ import Layout from '../components/MyLayout.js'
 import {getCampInfo} from '../lib/utils.js';
 import React from "react";
 
-    class Home extends React.Component {
-        constructor(props) {
-        super(props);
+  
         this.state = { search: "" };
-        }
+        
         
         
         
