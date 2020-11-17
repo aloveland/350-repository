2b07b97baebd4eeddc53e89df324f06a0c80f638
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-17 12:18:10 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -51,7 +51,7 @@ async handleSearch(evt) {
           <div>
 		<br />
   		<h2>{this.state.campInfo.name}</h2>
-	 	<h3>{this.state.campInfo.closest_town} <br/>{this.state.campInfo.description</h3>
+	 	<h3>{this.state.campInfo.closest_town} <br/>{this.state.campInfo.description}</h3>
           </div> : null}
 
         
