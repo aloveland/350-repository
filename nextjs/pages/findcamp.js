293492714b783ebede83e63a1ae4347023077f09
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-17 01:41:48 -0500

    Update findcamp.js

--- nextjs/pages/findcamp.js
+++ nextjs/pages/findcamp.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {getCampInfo} from '../lib/utils.js';
+const searchBar = document.getElementById('searchBar');
+
+
 
  export default function SearchBox(props){
 	
 	return(
 		<input type='text'
 		placeholder='search'
+		id='searchBar'
 		/>
 
 
