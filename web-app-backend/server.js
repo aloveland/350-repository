a1b1daf4fb5d9fdc242eb538a4a9267354d2bc5c
aloveland <44244521+aloveland@users.noreply.github.com>
2020-11-30 01:07:26 -0500

    Update server.js

--- web-app-backend/server.js
+++ web-app-backend/server.js
@@ -33,6 +33,7 @@ app.get('/hello', (req, res) => {
 app.get("/search",async (req, res) => {
     console.log("here");
     const term = req.query.term;
+    console.log(term);
     try {
        const template = "SELECT * FROM entries WHERE CONTAINS(description, $1)";
        const check = await pool.query(template,[term]);
