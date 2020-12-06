858716a225393bd4fe9f5cd99940d5ad4991193e
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-06 03:36:33 -0500

    Update server.js

--- web-app-backend/server.js
+++ web-app-backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const cors = requrie("cors);
+const cors = requrie("cors");
 
 const app = express();
 
