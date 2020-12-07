643441e57e370fc2f276d6da0e9ccb7b04324b70
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 02:12:31 -0500

    Update search.js

--- complete-app-frontend/SearchFunction/nextjs/pages/search.js
+++ complete-app-frontend/SearchFunction/nextjs/pages/search.js
@@ -44,7 +44,7 @@ async handleSearch(evt) {
 	  const campInfo = await getCampInfo(this.state.search);
 	  this.setState({campInfo});
 	  if(campInfo != null){
-	  	console.log(this.state.campInfo.description);
+	  	console.log(this.state.campInfo.desc);
 	  }
  	 }
   render() {
