b37a87807aa16cbf720e5d2b2eacfa63a045e892
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 15:55:40 -0500

    Update search.js

--- complete-app-frontend/SearchFunction/nextjs/pages/search.js
+++ complete-app-frontend/SearchFunction/nextjs/pages/search.js
@@ -5,17 +5,13 @@ import React from "react";
 var holdResults = [];
 var globalString = "";
 let answer = {};
-
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = { search: "" };
-    this.setState({search: evt.target.value});
-	      if(this.state.search == ""){
-		console.log("its happening JJ");
-
-	}  
-	  
+    this.setState({search: evt.target.value}); if(this.state.search == ""){ 
+	    console.log("its happening JJ"); 
+    }
   }	
 async handleSearch(evt) {
    console.log("being queried");
@@ -74,8 +70,7 @@ async handleSearch(evt) {
   		
 
 	this.handleSearch.bind(this.state.search);
-	
-	  
+	this.setState({search: evt.target.value});
 	  const campInfo = await getCampInfo(this.state.search);
 	    if(campInfo.error == "query failed"){
 	     console.log("its happening");
@@ -177,180 +172,22 @@ async handleSearch(evt) {
 			    </tr>
 			  </thead>
 			  <tbody>
-			        <tr>
-			      <td>{this.state.campInfo.result[0].desc}</td>
+			    <tr>
+			      <td>{this.state.campInfo.result[23].desc}</td>
 			      <td>{this.state.campInfo.result[0].kcal}</td>
 			      <td>{this.state.campInfo.result[0].protein}</td>
 	 		      <td>{this.state.campInfo.result[0].carbs}</td>
 	 		      <td>{this.state.campInfo.result[0].fat}</td>
 			    </tr>
 			    <tr>
-			       <td>{this.state.campInfo.result[1].desc}</td>
-			      <td>{this.state.campInfo.result[1].kcal}</td>
-			      <td>{this.state.campInfo.result[1].protein}</td>
-	 		      <td>{this.state.campInfo.result[1].carbs}</td>
-	 		      <td>{this.state.campInfo.result[1].fat}</td>
+			      <td>Sue</td>
+			      <td>00002</td>
+			      <td>Red</td>
 			    </tr>
 			    <tr>
-			      <td>{this.state.campInfo.result[2].desc}</td>
-			      <td>{this.state.campInfo.result[2].kcal}</td>
-			      <td>{this.state.campInfo.result[2].protein}</td>
-	 		      <td>{this.state.campInfo.result[2].carbs}</td>
-	 		      <td>{this.state.campInfo.result[2].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[3].desc}</td>
-			      <td>{this.state.campInfo.result[3].kcal}</td>
-			      <td>{this.state.campInfo.result[3].protein}</td>
-	 		      <td>{this.state.campInfo.result[3].carbs}</td>
-	 		      <td>{this.state.campInfo.result[3].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[4].desc}</td>
-			      <td>{this.state.campInfo.result[4].kcal}</td>
-			      <td>{this.state.campInfo.result[4].protein}</td>
-	 		      <td>{this.state.campInfo.result[4].carbs}</td>
-	 		      <td>{this.state.campInfo.result[4].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[5].desc}</td>
-			      <td>{this.state.campInfo.result[5].kcal}</td>
-			      <td>{this.state.campInfo.result[5].protein}</td>
-	 		      <td>{this.state.campInfo.result[5].carbs}</td>
-	 		      <td>{this.state.campInfo.result[5].fat}</td>
-			    </tr>
-			    <tr>
-			       <td>{this.state.campInfo.result[6].desc}</td>
-			      <td>{this.state.campInfo.result[6].kcal}</td>
-			      <td>{this.state.campInfo.result[6].protein}</td>
-	 		      <td>{this.state.campInfo.result[6].carbs}</td>
-	 		      <td>{this.state.campInfo.result[6].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[7].desc}</td>
-			      <td>{this.state.campInfo.result[7].kcal}</td>
-			      <td>{this.state.campInfo.result[7].protein}</td>
-	 		      <td>{this.state.campInfo.result[7].carbs}</td>
-	 		      <td>{this.state.campInfo.result[7].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[8].desc}</td>
-			      <td>{this.state.campInfo.result[8].kcal}</td>
-			      <td>{this.state.campInfo.result[8].protein}</td>
-	 		      <td>{this.state.campInfo.result[8].carbs}</td>
-	 		      <td>{this.state.campInfo.result[8].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[9].desc}</td>
-			      <td>{this.state.campInfo.result[9].kcal}</td>
-			      <td>{this.state.campInfo.result[9].protein}</td>
-	 		      <td>{this.state.campInfo.result[9].carbs}</td>
-	 		      <td>{this.state.campInfo.result[9].fat}</td>
-			    </tr>
-                              <tr>
-			      <td>{this.state.campInfo.result[10].desc}</td>
-			      <td>{this.state.campInfo.result[10].kcal}</td>
-			      <td>{this.state.campInfo.result[10].protein}</td>
-	 		      <td>{this.state.campInfo.result[10].carbs}</td>
-	 		      <td>{this.state.campInfo.result[10].fat}</td>
-			    </tr>
-			    <tr>
-			       <td>{this.state.campInfo.result[11].desc}</td>
-			      <td>{this.state.campInfo.result[11].kcal}</td>
-			      <td>{this.state.campInfo.result[11].protein}</td>
-	 		      <td>{this.state.campInfo.result[11].carbs}</td>
-	 		      <td>{this.state.campInfo.result[11].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[12].desc}</td>
-			      <td>{this.state.campInfo.result[12].kcal}</td>
-			      <td>{this.state.campInfo.result[12].protein}</td>
-	 		      <td>{this.state.campInfo.result[12].carbs}</td>
-	 		      <td>{this.state.campInfo.result[12].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[13].desc}</td>
-			      <td>{this.state.campInfo.result[13].kcal}</td>
-			      <td>{this.state.campInfo.result[13].protein}</td>
-	 		      <td>{this.state.campInfo.result[13].carbs}</td>
-	 		      <td>{this.state.campInfo.result[13].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[14].desc}</td>
-			      <td>{this.state.campInfo.result[14].kcal}</td>
-			      <td>{this.state.campInfo.result[14].protein}</td>
-	 		      <td>{this.state.campInfo.result[14].carbs}</td>
-	 		      <td>{this.state.campInfo.result[14].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[15].desc}</td>
-			      <td>{this.state.campInfo.result[15].kcal}</td>
-			      <td>{this.state.campInfo.result[15].protein}</td>
-	 		      <td>{this.state.campInfo.result[15].carbs}</td>
-	 		      <td>{this.state.campInfo.result[15].fat}</td>
-			    </tr>
-			    <tr>
-			       <td>{this.state.campInfo.result[16].desc}</td>
-			      <td>{this.state.campInfo.result[16].kcal}</td>
-			      <td>{this.state.campInfo.result[16].protein}</td>
-	 		      <td>{this.state.campInfo.result[16].carbs}</td>
-	 		      <td>{this.state.campInfo.result[16].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[17].desc}</td>
-			      <td>{this.state.campInfo.result[17].kcal}</td>
-			      <td>{this.state.campInfo.result[17].protein}</td>
-	 		      <td>{this.state.campInfo.result[17].carbs}</td>
-	 		      <td>{this.state.campInfo.result[17].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[18].desc}</td>
-			      <td>{this.state.campInfo.result[18].kcal}</td>
-			      <td>{this.state.campInfo.result[18].protein}</td>
-	 		      <td>{this.state.campInfo.result[18].carbs}</td>
-	 		      <td>{this.state.campInfo.result[18].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[19].desc}</td>
-			      <td>{this.state.campInfo.result[19].kcal}</td>
-			      <td>{this.state.campInfo.result[19].protein}</td>
-	 		      <td>{this.state.campInfo.result[19].carbs}</td>
-	 		      <td>{this.state.campInfo.result[19].fat}</td>
-			    </tr>
-			       <tr>
-			       <td>{this.state.campInfo.result[20].desc}</td>
-			      <td>{this.state.campInfo.result[20].kcal}</td>
-			      <td>{this.state.campInfo.result[20].protein}</td>
-	 		      <td>{this.state.campInfo.result[20].carbs}</td>
-	 		      <td>{this.state.campInfo.result[20].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[21].desc}</td>
-			      <td>{this.state.campInfo.result[21].kcal}</td>
-			      <td>{this.state.campInfo.result[21].protein}</td>
-	 		      <td>{this.state.campInfo.result[21].carbs}</td>
-	 		      <td>{this.state.campInfo.result[21].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[22].desc}</td>
-			      <td>{this.state.campInfo.result[22].kcal}</td>
-			      <td>{this.state.campInfo.result[22].protein}</td>
-	 		      <td>{this.state.campInfo.result[22].carbs}</td>
-	 		      <td>{this.state.campInfo.result[22].fat}</td>
-			    </tr>
-			    <tr>
-			      <td>{this.state.campInfo.result[23].desc}</td>
-			      <td>{this.state.campInfo.result[23].kcal}</td>
-			      <td>{this.state.campInfo.result[23].protein}</td>
-	 		      <td>{this.state.campInfo.result[23].carbs}</td>
-	 		      <td>{this.state.campInfo.result[23].fat}</td>
-			    </tr>
-			     <tr>
-			      <td>{this.state.campInfo.result[24].desc}</td>
-			      <td>{this.state.campInfo.result[24].kcal}</td>
-			      <td>{this.state.campInfo.result[24].protein}</td>
-	 		      <td>{this.state.campInfo.result[24].carbs}</td>
-	 		      <td>{this.state.campInfo.result[24].fat}</td>
+			      <td>Barb</td>
+			      <td>00003</td>
+			      <td>Green</td>
 			    </tr>
 			  </tbody>
 			</table></h2>
