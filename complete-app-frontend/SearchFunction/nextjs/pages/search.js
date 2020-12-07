2a7a41b3f90d7a449cbb389e22dcb5df87f31551
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 02:39:18 -0500

    Update search.js

--- complete-app-frontend/SearchFunction/nextjs/pages/search.js
+++ complete-app-frontend/SearchFunction/nextjs/pages/search.js
@@ -49,7 +49,7 @@ async handleSearch(evt) {
 	    var obj1 = {};
 	    obj1 = campInfo;
 	    console.log("777");
-	    console.log(obj1[1].desc);
+	    console.log(obj1);
  	 }
   render() {
     return (
