1f59370eb881020e33c5030100271a61cca1661a
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 23:57:15 -0500

    Update utils.js

--- complete-app-frontend/SearchFunction/nextjs/lib/utils.js
+++ complete-app-frontend/SearchFunction/nextjs/lib/utils.js
@@ -3,7 +3,7 @@ require("isomorphic-fetch");
 
 
 
-function getCampInfo(name) {
+function getFoodInfo(name) {
   console.log("Fetching");
   console.log("this is name " + name);
   return fetch(`http://34.106.215.20/api/search?term=${name}`).then(function(resp) {
@@ -17,7 +17,7 @@ function handleError(error) {
 }
 
 module.exports = {
-  getCampInfo: function(camp) {
-    return getCampInfo(camp).catch(handleError);
+  getFoodInfo: function(camp) {
+    return getFoodInfo(camp).catch(handleError);
   },
 };
