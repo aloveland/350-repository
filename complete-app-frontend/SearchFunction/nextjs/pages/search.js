0341f07dd720214f63f91af62702a59bdd7205b8
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 23:30:57 -0500

    Update search.js

--- complete-app-frontend/SearchFunction/nextjs/pages/search.js
+++ complete-app-frontend/SearchFunction/nextjs/pages/search.js
@@ -141,7 +141,7 @@ async handleSearch(evt) {
           textAlign: "center",
         }}
       >	
-	<img src= "/static/newmexico.jpg" className ="App-logo" />
+	<img src= "/static/nutrient-dense-foods.jpg" className ="App-logo" />
         <h1>Food Search</h1>
       
         <p>
