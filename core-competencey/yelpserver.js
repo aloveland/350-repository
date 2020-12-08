0190b860fbd33331a5443b4eb1e43f88607da0f9
aloveland <44244521+aloveland@users.noreply.github.com>
2020-12-07 21:07:23 -0500

    Update yelpserver.js

--- core-competencey/yelpserver.js
+++ core-competencey/yelpserver.js
@@ -164,7 +164,7 @@ app.get("/find",async (req, res) => {
  
     try {
         const template = "SELECT * FROM restaurant WHERE tpye = $1";
-        const check = await pool.query(template, [tpye]);
+        const check = await pool.query(template, [type]);
         if (check.rowCount == 0){
             res.json({status: 'no restaurants of this type'});
              }
